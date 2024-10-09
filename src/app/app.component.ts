305d694e38382388982a3c1051644281d34d7d64
Sami Ullah <sami.ullah1@g42.ai>
2024-10-09 10:14:11 +0400

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HAND_IMAGE } from './images/hand';
+import { FLOWER_IMAGE } from './images/flower';
+import { AiaImageAnnotatorComponent, AngularImageAnnotatorModule } from '@nx-angular-image-annotator/aia';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AngularImageAnnotatorModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'nx-angular-image-annotator';
+  base64Image = FLOWER_IMAGE;
+  public strokeColor = '#000';
+  public fontSize = '20px';
+  public fontFamily = 'Georgia';
+
+  @ViewChild('annotator') annotator!: AiaImageAnnotatorComponent;
+
+  public getAnnotatedImage() {
+    const annotatedImageURI = this.annotator.getAnnotatedImage();
+    const downloadURL = annotatedImageURI.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
+    window.open(downloadURL);
+
+  }
+
+  public changeImage(image: any) {
+    if (image === 'flower') {
+      this.base64Image = FLOWER_IMAGE;
+    } else {
+      this.base64Image = HAND_IMAGE;
+    }
+  }
+
+  public pencilTool() {
+    this.annotator.setTool('pencil');
+  }
+
+  public textTool() {
+    this.annotator.setTool('text');
+  }
+
+  public undo() {
+    this.annotator.undo();
+  }
+
+  public redo() {
+    this.annotator.redo();
+  }
+
+  public clear() {
+    this.annotator.clear();
+  }
 }
